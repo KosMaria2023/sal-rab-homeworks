548881f2620d667759a880311f79304048994204
Maria Kostroma <mariakostroma@gmail.com>
2023-04-10 19:58:40 +0300

    task 1- done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,6 +2,18 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message +  " ";
+    message = message +  productPrice;
+    message = message +  " ";
+    message = message + "теперь в корзине!";
+    console.log(message)
+
+
+   
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
@@ -20,16 +32,18 @@ function addInCartMessage(title, price) {
 }
 
 function addInCartCountChange(value) {
-
+ let oldValue = value;
     // Задание №1.2. Увеличение количества в корзине
-    let oldValue = value;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
 
     // Конец решения задания №1.2.
-
+ 
+    let newValue;
+    newValue = oldValue;
+    newValue += 1;
     return newValue;
 }
 
@@ -44,7 +58,9 @@ function addInCartSumChange(sum, delta) {
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
     // Конец решения задания №1.3.
-
+    let newSum = oldSum + difference;
+    let newSumText = newSum +  ' ₽';
+    newSumText = `${newSum} ₽`;
     return newSumText;
 }
 
